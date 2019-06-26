a806bd61bcd5e1479ec8fa8245d46b9a8a09e6ff
Isabella Teixeira <isabellamteixeira@gmail.com>
2019-06-25 21:17:40 -0300

    js ok. working on front-end

--- js/main.js
+++ js/main.js
@@ -1,19 +1,22 @@
+// função para gerar elementos no DOM
 function gerarPizza() {
 	var containerPizzas = document.querySelector('#container-pizzas');
 	//<li></li>
 	var pizza = document.createElement('li'); 
+	pizza.className = "list-inline-item";
 	//<img src>
 	var pizzaImg = document.createElement('img');
 	//<img src="..">
-	pizzaImg.setAttribute('src', 'https://source.unsplash.com/100x100/?pizza');
+	pizzaImg.setAttribute('src', 'imgs/pizza.png');
 	pizzaImg.className = "borda-redonda";
 	//<li><img></li>
 	pizza.appendChild(pizzaImg); 
 	containerPizzas.appendChild(pizza);
 }
-
+//coletando o botão
 var botao = document.querySelector('button');
 
+//gerando pizzas no evento onclick
 botao.onclick = function(){
 	gerarPizza();
 }
